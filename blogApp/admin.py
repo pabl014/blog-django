b1b1ecc602bbf17278c97753837352dee4dc748c
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-04-16 23:19:28 +0200

    Create models

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Blog, Article, Comment
 
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(Article)
+admin.site.register(Comment)
--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class CustomUser(AbstractUser):
+#     avatar = models.CharField(max_length=100, blank=True)
+#     isAdmin = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.username
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    public = models.BooleanField(default=False)
+    password = models.CharField(max_length=100, blank=True)
+    date = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.article.title}"
